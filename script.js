cd7722f3961a88d4133a904c717550b5fc100b78
ClydeKaiser <152431717+ClydeKaiser@users.noreply.github.com>
2023-12-05 11:49:18 -0800

    update - canvas, ol, images

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-// draw a road at bottom with js canvas
+// Ensure the DOM is fully loaded before accessing elements
+        document.addEventListener('DOMContentLoaded', function () {
+            // Get the canvas element
+            var canvas = document.getElementById('roadCanvas');
+            var ctx = canvas.getContext('2d');
+
+            //road color
+            var roadColor = '#808080';
+
+            //road
+            function drawRoad() {
+                // Draw the road background
+                ctx.fillStyle = roadColor;
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+                //road markings
+                ctx.fillStyle = 'yellow'; 
+                var markingWidth = 30; 
+                var markingGap = 50; 
+
+                // Draw the center line
+                for (var i = 0; i < canvas.width; i += markingGap + markingWidth) {
+                    ctx.fillRect(i, canvas.height / 2 - markingWidth / 2, markingWidth, markingWidth);
+                }
+            }
+             function drawCar(x, y) {
+                // Car body
+                ctx.fillStyle = 'red';
+                ctx.fillRect(x, y, 80, 40);
+
+                // Car roof
+                ctx.fillStyle = 'blue';
+                ctx.fillRect(x + 10, y - 20, 60, 20);
+
+                // Wheels
+                ctx.fillStyle = 'black';
+                ctx.beginPath();
+                ctx.arc(x + 20, y + 40, 10, 0, 2 * Math.PI);
+                ctx.arc(x + 60, y + 40, 10, 0, 2 * Math.PI);
+                ctx.fill();
+            }
+
+            // Draw the road
+            drawRoad();
+
+            // Draw the car on the road
+            drawCar(100, canvas.height - 60)
+        });
