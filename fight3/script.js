24b406b602d05faa45fce2a4df14970f6da71bd0
ClydeKaiser <152431717+ClydeKaiser@users.noreply.github.com>
2024-05-30 09:29:30 -0700

    Create script.js

---
+++ fight3/script.js
@@ -0,0 +1,7 @@
+var amount = "";
+document.getElementById("betBtn").addEventListener("click", function money(){
+  amount = prompt("Enter the amount you would like to Bet", "$00.00")
+  if (amount != null && amount != "$00.00"){
+  alert("Your bet has been placed! Thank you for your buisnesss!")
+}
+});
